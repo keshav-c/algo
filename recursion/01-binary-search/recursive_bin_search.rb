3fa0cc72d441672af2d9dcdb1ee9810339029f95
keshav-c <keshav1985@gmail.com>
2019-06-14 14:10:51 +0530

    Make sqrt_recursive work for non perfect squares

--- recursion/01-binary-search/recursive_bin_search.rb
+++ recursion/01-binary-search/recursive_bin_search.rb
@@ -1,18 +1,19 @@
 def sqrt(number)
-  sqrt_recursive(number, 0, number)
+    sqrt_recursive(number, 0, number)
 end
 
-def sqrt_recursive(number, min_interval, max_interval)
-  mid = (min_interval + max_interval) / 2
-  # puts "Min: #{min_interval}; Mid: #{mid}; Max: #{max_interval}"
-  if number == mid ** 2
-    return mid
-  elsif number > (mid ** 2)
-    return sqrt_recursive(number, mid, max_interval)
-  else
-    return sqrt_recursive(number, min_interval, mid)
-  end
+def sqrt_recursive(number, min_interval, max_interval, delta = 10 ** -10)
+    mid = (min_interval + max_interval) / 2.0
+    puts "Min: #{min_interval}; Mid: #{mid}; Max: #{max_interval}"
+    if (number >= (mid ** 2) - delta) && (number <= (mid ** 2) + delta)
+        puts "Square Root of #{number} = #{mid}; And mid^2 = #{mid ** 2}"
+        return mid
+    elsif number > ((mid ** 2) + delta)
+        return sqrt_recursive(number, mid, max_interval)
+    else
+        return sqrt_recursive(number, min_interval, mid)
+    end
 end
 
-puts sqrt(25)
-puts sqrt(7056)
+puts sqrt(3.33)
+# puts sqrt(7056)
