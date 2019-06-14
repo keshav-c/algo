f22523fe2f3d7d10f72924c531a758ceefaaf530
keshav-c <keshav1985@gmail.com>
2019-06-14 14:13:48 +0530

    Remove debugging lines from recursive_bin_search.rb

--- recursion/01-binary-search/recursive_bin_search.rb
+++ recursion/01-binary-search/recursive_bin_search.rb
@@ -4,9 +4,7 @@ end
 
 def sqrt_recursive(number, min_interval, max_interval, delta = 10 ** -10)
     mid = (min_interval + max_interval) / 2.0
-    puts "Min: #{min_interval}; Mid: #{mid}; Max: #{max_interval}"
     if (number >= (mid ** 2) - delta) && (number <= (mid ** 2) + delta)
-        puts "Square Root of #{number} = #{mid}; And mid^2 = #{mid ** 2}"
         return mid
     elsif number > ((mid ** 2) + delta)
         return sqrt_recursive(number, mid, max_interval)
@@ -16,4 +14,5 @@ def sqrt_recursive(number, min_interval, max_interval, delta = 10 ** -10)
 end
 
 puts sqrt(3.33)
-# puts sqrt(7056)
+puts sqrt(7056)
+puts sqrt(25)
