7ab59791a53b600371bb4a3702de35fa71af4b17
keshav-c <keshav1985@gmail.com>
2020-06-03 00:58:31 +0530

    Solve the shortest path problem, Intro costs in wizard conference

--- trees-and-graphs/02-graphs/lib/wizard_intro.rb
+++ trees-and-graphs/02-graphs/lib/wizard_intro.rb
@@ -2,7 +2,41 @@ def get_min_magic(src, dest, wizards)
   minCost = 0
   minPath = []
 
+  minCosts = Array.new(10, Float::INFINITY)
+  prev = Array.new(10)
+  visited = Array.new(10, false)
   
+  minCosts[src] = 0
+  currentWiz = src
+  loop do
+    # Shortest path algorithm
+    wizards[currentWiz].digits(10).each do |reference|
+      if !visited[reference]
+        cost_from_current = minCosts[currentWiz] + (currentWiz - reference) ** 2
+        if cost_from_current < minCosts[reference]
+          minCosts[reference] = cost_from_current
+          prev[reference] = currentWiz
+        end
+      end
+    end
+    visited[currentWiz] = true
+    # Choose next wizard
+    smallestCost = Float::INFINITY
+    currentWiz = nil
+    minCosts.each_with_index do |referenceCost, wizard|
+      if !visited[wizard] && referenceCost < smallestCost
+        smallestCost = referenceCost
+        currentWiz = wizard
+      end
+    end
+    break if currentWiz == dest
+  end
+  # Results
+  minCost = minCosts[currentWiz]
+  until currentWiz == nil
+    minPath.unshift(currentWiz)
+    currentWiz = prev[currentWiz]
+  end
 
   return minCost, minPath
 end
--- trees-and-graphs/02-graphs/spec/wizard_intro_spec.rb
+++ trees-and-graphs/02-graphs/spec/wizard_intro_spec.rb
@@ -7,6 +7,6 @@ describe '#get_min_magic' do
     refs = [123, 864, 783, 81, 6, 87, 94, 46, 1, 14]
     cost, path = get_min_magic(w1, w2, refs)
     expect(cost).to be 23
-    expect(path).to be [0, 1, 4, 6, 9]
+    expect(path).to eq [0, 1, 4, 6, 9]
   end
 end
